8f31033a3f7b7f5ebf378c5c3604cfed04f2b8e9
HarshShinde2697 <harshshinde232@gmail.com>
2026-03-20 23:19:04 +0530

    This changes through app file

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 app =Flask(__name__)
 @app.route('/',methods=['GET'])
 def home():
-    return "Hello Docker"
+    return "Hello Docker is here"
 
 if __name__=='__main__':
     app.run(debug=True,host="0.0.0.0",port=5000)
