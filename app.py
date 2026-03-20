12f14dcb29a217f3631a37bdced7febff498e204
HarshShinde2697 <harshshinde232@gmail.com>
2026-03-20 23:19:43 +0530

    this is third commit

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import os
 app =Flask(__name__)
 @app.route('/',methods=['GET'])
 def home():
-    return "Hello Docker is here"
+    return "Hello Docker is here" \
+    ""
 
 if __name__=='__main__':
     app.run(debug=True,host="0.0.0.0",port=5000)
